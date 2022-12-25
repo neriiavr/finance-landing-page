8cd9143ea4670d32001ccd1e455127c6fbc35235
neriiavr <avrupalineriman@gmail.com>
2022-12-25 15:25:29 +0100

    Newsletter initial implementation

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,18 @@
+import React from 'react' 
+
+const Newsletter = () => {
+
+    return (
+        <div className='w-full py-16 text-white '>
+            <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'> 
+            <div>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want tips & tricks to optimize your flow? </h1>
+                <p>Sign up to our newsletter and stay up to date.</p>
+            </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Newsletter 
