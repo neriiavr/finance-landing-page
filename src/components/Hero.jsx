928a211808e394adb3ef6270e10103f95b140993
neriiavr <avrupalineriman@gmail.com>
2022-12-26 05:39:54 +0100

    Cards and Footer part added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                 <p className="md:text-2xl text-xl font-bold text-gray-500">
                     Monitor your data analytics to increase revenue for BTB, BTC & SAAS platforms.
                 </p>
-                <button className="bg-[#00df9a] w-[200px] rounded-md my-6 mx-auto py-3 text-black">Get Started</button>
+                <button className="bg-[#00df9a] w-[200px] rounded-md my-6 mx-auto py-3 font-medium text-black">Get Started</button>
             </div>
         </div>
     )
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,12 +3,19 @@ import React from 'react'
 const Newsletter = () => {
 
     return (
-        <div className='w-full py-16 text-white '>
+        <div className='w-full py-16 text-white px-4'>
             <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'> 
-            <div>
+            <div className='lg:cool-span-2 my-4'>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want tips & tricks to optimize your flow? </h1>
                 <p>Sign up to our newsletter and stay up to date.</p>
             </div>
+            <div className='my-4'>
+                <div className="flex flex-col items-center justify-between w-full">
+                    <input className="p-3 flex w-full rounded-md text-black" type="email" placeholder="Enter Email" />
+                    <button className="bg-[#00df9a] w-[200px] rounded-md my-6 ml-4 mx-auto py-3 text-black font-medium">Notify Me</button>
+                </div>
+            </div>
+            <p className="py-3 ml-4">We care about the protection of your data. Read our <span className="text-[#00df9a]"> Privacy Policy </span> . </p>
             </div>
 
         </div>
